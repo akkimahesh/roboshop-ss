a60a2f251876a1443a3790e62541d94aa3c642c6
akkimahesh <maheshakki799@gmail.com>
2025-11-07 00:00:47 +0530

    redis-ss

---
+++ redis.sh
@@ -0,0 +1,34 @@
+ID=$(id -u)
+
+TIMESTAMP=$(date +%F-%H%M)
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+
+if [ "$ID" -ne 0 ]
+then
+    echo "Please run as root"
+    exit 1
+else
+    echo "Running as root"
+fi
+
+VALIDATE() {
+    if [ $1 -ne 0 ]
+    then
+        echo "ERROR:: $2  failed"
+        exit 1
+    else
+        echo "SUCCESS:: $2 success"
+    fi  
+}
+
+apt update -y &>> $LOGFILE
+VALIDATE $? "Updating apt repos"
+
+apt install redis-server -y &>> $LOGFILE
+VALIDATE $? "Installing Redis Server"
+
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/redis/redis.conf
+VALIDATE $? "Configuring Redis to listen on all interfaces"
+
+systemctl restart redis &>> $LOGFILE
+VALIDATE $? "Restarting Redis Service"
